1454a8f9858f6fbcec946a193eeb575a6646a79c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-09-15 16:23:10 +0200

    Show coverage area (300m, 600m)

--- src/index.js
+++ src/index.js
@@ -8,4 +8,24 @@ window.onload = function () {
   L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
+  let coverageLayer1 = L.TileLayer.maskCanvas({
+    radius: 600,
+    useAbsoluteRadius: true,
+    opacity: 0.5
+  })
+  coverageLayer1.setData([
+    [ 51.5, -0.09 ]
+  ])
+  map.addLayer(coverageLayer1)
+
+  let coverageLayer2 = L.TileLayer.maskCanvas({
+    radius: 300,
+    useAbsoluteRadius: true,
+    opacity: 0.5
+  })
+  coverageLayer2.setData([
+    [ 51.5, -0.09 ]
+  ])
+  map.addLayer(coverageLayer2)
 }

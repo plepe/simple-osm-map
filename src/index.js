09e0be353b4b50da839db1801eaf9aeb978bae26
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-09-15 16:53:40 +0200

    Load data from schlosspenz.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const httpGet = require('./httpGet')
+const async = {
+  each: require('async/each'),
+  eachLimit: require('async/eachLimit')
+}
+
 window.onload = function () {
   var map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -14,9 +20,6 @@ window.onload = function () {
     useAbsoluteRadius: true,
     opacity: 0.5
   })
-  coverageLayer1.setData([
-    [ 51.5, -0.09 ]
-  ])
   map.addLayer(coverageLayer1)
 
   let coverageLayer2 = L.TileLayer.maskCanvas({
@@ -24,8 +27,64 @@ window.onload = function () {
     useAbsoluteRadius: true,
     opacity: 0.5
   })
-  coverageLayer2.setData([
-    [ 51.5, -0.09 ]
-  ])
   map.addLayer(coverageLayer2)
+
+  let routes = []
+  let elements = {}
+  let coverageData = {}
+
+  httpGet('schlosspenz_aktuell.json', {}, (err, result) => {
+    if (err) {
+      console.log(err)
+      return alert("Can't download geojson file")
+    }
+
+    result = JSON.parse(result.body)
+
+    async.eachLimit(result.elements, 32,
+      (element, done) => {
+        let id = element.type + '/' + element.id
+        elements[id] = element
+
+        if (element.type === 'relation' && element.tags && element.tags.type === 'route') {
+          routes.push(element)
+        }
+
+        done()
+      },
+      (err) => {
+        async.eachLimit(routes, 4,
+          (route, done) => {
+            async.each(route.members,
+              (member, done) => {
+                let memberId = member.type + '/' + member.ref
+                let element = elements[memberId]
+
+                if (!element) {
+                  console.log("Can't find element " + memberId)
+                  return done()
+                }
+
+                if (member.role === 'stop') {
+                  if (!(memberId in coverageData)) {
+                    coverageData[memberId] = [ element.lat, element.lon ]
+                  }
+                }
+
+                done()
+              },
+              (err) => {
+                done(err)
+              }
+            )
+          },
+          (err) => {
+            let data = Object.values(coverageData)
+            coverageLayer1.setData(data)
+            coverageLayer2.setData(data)
+          }
+        )
+      }
+    )
+  })
 }
